717c44acfba5e2f0490b133c4ee3a751019562a5
Adam Weesner <alweesner@gmail.com>
2020-09-12 19:04:29 -0500

    added vectors in favor over point3d and pointf3d

---
+++ app/src/main/java/com/weesnerdevelopment/playingwithgames/natureOfCode/objects/Vector.kt
@@ -0,0 +1,56 @@
+package com.weesnerdevelopment.playingwithgames.natureOfCode.objects
+
+import math.absoluteValue
+import math.minus
+import math.plus
+
+sealed class Vec(
+) {
+    open lateinit var x: Number
+    open lateinit var y: Number
+
+    abstract operator fun plus(other: Vec): Vec
+    abstract fun minus(other: Vec): Vec
+
+    open fun toAbs(): Vec = apply {
+        x = x.absoluteValue
+        y = y.absoluteValue
+    }
+}
+
+data class Vector(
+    override var x: Number,
+    override var y: Number
+) : Vec() {
+    override operator fun plus(other: Vec): Vector = Vector(
+        x + other.x,
+        y + other.y
+    )
+
+    override fun minus(other: Vec): Vector = Vector(
+        x - other.x,
+        y - other.y
+    )
+}
+
+data class Vector3D(
+    override var x: Number,
+    override var y: Number,
+    var z: Number
+) : Vec() {
+    override operator fun plus(other: Vec): Vector3D = Vector3D(
+        x + other.x,
+        y + other.y,
+        if (other is Vector3D) z + other.z else z
+    )
+
+    override fun minus(other: Vec): Vector3D = Vector3D(
+        x - other.x,
+        y - other.y,
+        if (other is Vector3D) z - other.z else z
+    )
+
+    override fun toAbs(): Vector3D = super.toAbs().apply {
+        z = z.absoluteValue
+    } as Vector3D
+}
---
+++ app/src/test/java/com/weesnerdevelopment/playingwithgames/VectorTests.kt
@@ -0,0 +1,43 @@
+package com.weesnerdevelopment.playingwithgames
+
+import com.weesnerdevelopment.playingwithgames.natureOfCode.objects.Vector
+import com.weesnerdevelopment.playingwithgames.natureOfCode.objects.Vector3D
+import org.junit.Assert
+import org.junit.Test
+
+class VectorTests {
+    @Test
+    fun `adding two vectors`() {
+        val first = Vector(1, 1)
+        val second = Vector(1, 1)
+
+        Assert.assertEquals(Vector(2, 2), first + second)
+    }
+
+    @Test
+    fun `adding two 3d vectors`() {
+        val first = Vector3D(1, 1, 1)
+        val second = Vector3D(1, 1, 1)
+
+
+        Assert.assertEquals(Vector3D(2, 2, 2), first + second)
+    }
+
+    @Test
+    fun `adding 2d and 3d vectors`() {
+        val first = Vector(1, 1)
+        val second = Vector3D(1, 1, 1)
+
+
+        Assert.assertEquals(Vector(2, 2), first + second)
+    }
+
+    @Test
+    fun `adding 3d and 2d vectors`() {
+        val first = Vector(1, 1)
+        val second = Vector3D(1, 1, 1)
+
+
+        Assert.assertEquals(Vector3D(2, 2, 1), second + first)
+    }
+}
