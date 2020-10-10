de9faca24527139301b23fa44b38142af0165f0d
Adam Weesner <alweesner@gmail.com>
2020-10-10 02:06:02 -0500

    updated app module with changes

--- app/src/main/java/com/weesnerDevelopment/playingWithGames/MainActivity.kt
+++ app/src/main/java/com/weesnerDevelopment/playingWithGames/MainActivity.kt
@@ -1,40 +1,40 @@
 package com.weesnerDevelopment.playingWithGames
 
-import android.content.Context
-import android.content.res.Resources
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.weesnerDevelopment.mrNom.MrNomGame
-import com.weesnerDevelopment.playingWithGames.game.StateVariables
-import com.weesnerDevelopment.playingWithGames.natureOfCode.ballFollowTouch.BallFollowTouchFragment
-import com.weesnerDevelopment.playingWithGames.natureOfCode.ballWithForces.BallWithForcesFragment
-import com.weesnerDevelopment.playingWithGames.natureOfCode.bouncingBall.BouncingBallFragment
-import com.weesnerDevelopment.playingWithGames.natureOfCode.perlinBackground.PerlinBackgroundFragment
-import com.weesnerDevelopment.playingWithGames.natureOfCode.randomWalker.WalkerFragment
-import com.weesnerDevelopment.playingWithGames.random.bouncingBall.BallBounceFragment
-import com.weesnerDevelopment.playingWithGames.random.circlePhysics.CirclePhysicsFragment
-import com.weesnerDevelopment.playingWithGames.random.circlePhysics.collision.CircleCollisionPhysicsFragment
+import androidx.lifecycle.lifecycleScope
+import com.weesnerDevelopment.randomGameEngine.game.StateVariables
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val lastIndex = resources.getStringArray(R.array.fragments).lastIndex
+        lifecycleScope.launch {
+            StateVariables.currentFragment.flow.collect {
+                StateVariables.currentGameView.value?.stop()
 
-        fragments(resources, 1)?.let {
-            StateVariables.currentFragment.value = 1
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.main_layout, it, it::class.simpleName)
-                .commit()
+                fragments(resources, it)?.let {
+                    StateVariables.currentGameView.value?.stop()
+                    supportFragmentManager.apply {
+                        beginTransaction()
+                            .replace(
+                                R.id.main_layout,
+                                findFragmentByTag(it::class.simpleName) ?: it,
+                                it.tag
+                            )
+                            .addToBackStack(null)
+                            .commit()
+                    }
+                }
+            }
         }
-
     }
 
     override fun onBackPressed() {
-        if (supportFragmentManager.backStackEntryCount > 0){
+        if (supportFragmentManager.backStackEntryCount > 0) {
             supportFragmentManager.popBackStack()
         } else {
             super.onBackPressed()
